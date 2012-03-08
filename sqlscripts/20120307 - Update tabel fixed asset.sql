d30fa089c1d512ccda0841a8c52ea5110ee1c8ba
wisnu27 <wisnu27@gmail.com>
2012-03-08 11:10:03 +0700

    Update tabel Fixasset

---
+++ sqlscripts/20120307 - Update tabel fixed asset.sql
@@ -0,0 +1,6 @@
+﻿alter table transaction.fixedasset add buyername varchar(200);
+alter table transaction.fixedasset add assetdetaildescription varchar(500);
+alter table transaction.fixedasset add assetorigin varchar(1);
+
+insert into transaction.enum_varchar values('eAssetOrigin','D','Donasi');
+insert into transaction.enum_varchar values('eAssetOrigin','B','Pembelian');
